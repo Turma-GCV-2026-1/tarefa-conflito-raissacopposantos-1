8f7e00c4da52cbbb27aa9d4a3fe3ed6869bfd5c0
Raíssa Coppo Santos <raissa.coppo.santos@gmail.com>
2026-05-04 11:19:22 +0000

    Segunda att

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
     printf("Imprimindo linha 07.\n");
     printf("Imprimindo linha 08.\n");
     printf("Imprimindo linha 09.\n");
-    printf("Imprimindo linha 10.\n");
+    printf("Imprimindo linha 10. Segunda atualização\n");
+    printf("Imprimindo linha 11.")
     return 0;
 }
